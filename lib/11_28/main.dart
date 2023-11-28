b0d0e0a371610c3e1dd76c5b6647f5ee9efddee9
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-11-28 12:27:35 +0900

    examples

---
+++ lib/11_28/main.dart
@@ -0,0 +1,62 @@
+void main(){
+
+ int number = 10;
+
+ print(number);
+
+ number = 12;
+
+ print(number);
+
+ double p = 3.14;
+
+ print(p);
+
+ p = 2.17;
+
+ bool isHappy = false;
+
+ print(isHappy);
+
+ isHappy = true;
+
+ print(isHappy);
+
+ String name = 'Dart';
+
+ print(name);
+
+ name = 'Flutter';
+
+ print(name);
+
+ List<int> numbers = [1,2,3,4,5,6];
+
+ print(numbers);
+
+ numbers = [1,2,3,4,5];
+
+ print(numbers);
+
+ Map<String, int> ages = {
+   'Albert' : 25,
+   'Christy' : 26,
+   'Luna' : 20,
+ };
+
+ print(ages);
+
+ ages = {
+   'Albert' : 26,
+   'Christy' : 27,
+   'Luna' : 21,
+ };
+
+ print(ages);
+
+
+
+
+
+
+}
