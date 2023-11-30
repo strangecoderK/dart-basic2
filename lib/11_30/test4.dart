95a520e17049cb55e231dfcc981dff6e973750c9
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-01 02:47:05 +0900

    final

--- lib/11_30/test4.dart
+++ lib/11_30/test4.dart
@@ -1,17 +1,5 @@
 void main(){
-  /**int result = numbers(10);
-  for()
-  print(result);
 
-}
-int numbers(int a) {
-    return 5 + 10 * a;
-  }
-
-  int a=0;
-  while(a<=10){
-    print (result);
-    a++;**/
     int a=10;
     for(int i=0;i<=a;i++){
     print(5 + 10 * i);}
