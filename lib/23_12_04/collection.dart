2fd4d462878c7a5d38d3e9d15d4cf66db992fb40
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-05 04:27:00 +0900

    연습문제 2-2

--- lib/23_12_04/collection.dart
+++ lib/23_12_04/collection.dart
@@ -1,4 +1,16 @@
-void main() {}
-/**1) set
-    2) Map
-    3) Map **/
+void main() {
+
+  Person person1 = Person('홍길동',20);
+  Person person2 = Person('한석봉', 16);
+  List<Person> names = [person1, person2];
+
+  for(int i=0; i<names.length; i++){
+    print(names[i].name);
+  }
+}
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+}
