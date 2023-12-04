c3f9fc322ec5fe920673b7704d24b430da2f5289
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-05 03:53:12 +0900

    연습문제 10-2

--- lib/23_12_04/wizard.dart
+++ lib/23_12_04/wizard.dart
@@ -1,23 +1,86 @@
-void main() {}
+void main() {
+  Wizard wizard = Wizard(
+    name: '마법사',
+    hp: 50,
+    mp: 50,
+  );
+
+  wizard.name = '토르';
+  wizard.mp = -5;
+  wizard.hp = -4;
+  print(wizard._hp);
+
+  Wand wand = Wand(
+    name: '지팡이',
+    power: 60,
+  );
+  wand.name = '연필';
+  wand.power = 150;
+}
 
 class Wand {
-  String name;
-  double power;
+  String _name;
+  double _power;
 
   Wand({
-    required this.name,
-    required this.power,
-  });
+    required String name,
+    required double power,
+  })  : _name = name,
+        _power = power;
+
+  String get name => _name; //읽기 허용
+
+  set name(String value) {
+    _name = value;
+    if (value.length < 3) {
+      throw Exception('이름은 3보다 커야합니다.');
+    }
+  }
+
+  set power(double value) {
+    _power = value;
+    if (value < 0.5) {
+      throw Exception('마력이 약합니다.');
+    } else if (value > 105) {
+      throw Exception('마력을 감당하지 못합니다.');
+    }
+  }
 }
 
 class Wizard {
-  String name;
-  int hp;
+  String _name;
+  int _hp;
+  int _mp;
   Wand? wand;
 
   Wizard({
-    required this.name,
-    required this.hp,
+    required String name,
+    required int hp,
+    required int mp,
     this.wand,
-  });
+  })  : _name = name,
+        _hp = hp,
+        _mp = mp;
+
+  set name(String value) {
+    _name = value;
+    if (value.length < 3) {
+      throw Exception('이름은 3보다 커야합니다.');
+    }
+  }
+
+  set mp(int value) {
+    _mp = value;
+    if (value < 0) {
+      throw Exception('힘이 없다');
+    }
+  }
+
+  set hp(int value) {
+    if (value < 0) {
+      _hp = 0; // 음수일 경우 0으로 설정
+    } else {
+      _hp = value;
+    }
+  }
 }
