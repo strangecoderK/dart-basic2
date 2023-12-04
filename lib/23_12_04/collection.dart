9865712ed3bb3dbf4c4b720850beffa8c8435c0d
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-05 04:41:31 +0900

    연습문제 2-3

--- lib/23_12_04/collection.dart
+++ lib/23_12_04/collection.dart
@@ -1,13 +1,19 @@
 void main() {
-
-  Person person1 = Person('홍길동',20);
-  Person person2 = Person('한석봉', 16);
+  Person person1 = Person('홍길동', 20);
+  Person person2 = Person('한석봉', 25);
   List<Person> names = [person1, person2];
 
-  for(int i=0; i<names.length; i++){
+  for (int i = 0; i < names.length; i++) {
     print(names[i].name);
   }
+
+  Map<String, int> info = {'홍길동': 20, '한석봉': 25};
+
+  info.entries.forEach((element) {
+    print('${element.key}의 나이는 ${element.value}살');
+  });
 }
+
 class Person {
   String name;
   int age;
