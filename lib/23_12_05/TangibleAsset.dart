f01e517967abe9499139d988a49be1f3331728aa
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-06 01:13:28 +0900

    연습문제 12-3

--- lib/23_12_05/TangibleAsset.dart
+++ lib/23_12_05/TangibleAsset.dart
@@ -10,17 +10,5 @@ abstract class TangibleAsset extends Asset {
     required super.name,
     required super.price,
     required this.color,
-
   });
-
-  @override
-  double get weight {
-    return weight;
-  }
-
-  ;
-
-  set weight(double value);
-
-
 }
