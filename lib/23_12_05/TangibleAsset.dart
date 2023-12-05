31c10e599335779f5e24fda398520d9d2693f95f
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-05 16:54:42 +0900

    연습문제 12-2

--- lib/23_12_05/TangibleAsset.dart
+++ lib/23_12_05/TangibleAsset.dart
@@ -1,11 +1,15 @@
-abstract class TangibleAsset {
-  String name;
-  int price;
+//가 : asset
+//나 : IntangibleAsset
+//다 : Patent
+import 'package:untitled/23_12_05/asset.dart';
+
+abstract class TangibleAsset extends Asset{
+
   String color;
 
   TangibleAsset({
-    required this.name,
-    required this.price,
+    required super.name,
+    required super.price,
     required this.color,
   });
 }
