b0f64c7f155d0b0b5e062edddd6e2f039c3f0fd8
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-11 04:33:08 +0900

    연습문제 2

--- lib/23_12_08/strong_box.dart
+++ lib/23_12_08/strong_box.dart
@@ -5,15 +5,51 @@ enum KeyType {
   finger,
 }
 
+int initialCount(KeyType keyType) {
+  switch (keyType) {
+    case KeyType.padlock:
+      return 1024;
+    case KeyType.button:
+      return 10000;
+    case KeyType.dial:
+      return 30000;
+    case KeyType.finger:
+      return 1000000;
+  }
+}
+
 class StrongBox<E> {
-  E? _data;
+  E? _item;
   KeyType keyType;
+  int count;
 
-  StrongBox(this._data, this.keyType);
+  StrongBox(this.keyType) : count = initialCount(keyType);
 
-  void put(E data) {
-    _data = data;
+  void put(E item) {
+    _item = item;
   }
 
-  E? get() => _data;
+  E? get() {
+    if (count > 0) {
+      count--;
+      return null;
+    } else {
+      return _item;
+    }
+  }
+}
+
+void main() {
+  StrongBox<String> strongBox = StrongBox(KeyType.button);
+  strongBox.put("획득");
+
+  for (int i = 0; i < 10010; i++) {
+    dynamic result = strongBox.get();
+    if (result == null) {
+      print(" ${strongBox.count}회 남음. 접근불가");
+    } else {
+      print(result);
+      break;
+    }
+  }
 }
