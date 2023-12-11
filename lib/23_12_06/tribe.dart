32c8c02178562516395d0cd85beed2f9f9dc5801
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-12 03:12:45 +0900

    changes

--- lib/23_12_06/tribe.dart
+++ lib/23_12_06/tribe.dart
@@ -1,14 +1,1 @@
-import '../game/sword.dart';
-
-abstract class Tribe {}
-class Hero {
-  String name;
-  int hp;
-  Sword? sword;
-
-  Hero({
-    this.name='홍길동',
-    this.hp=100,
-    this.sword,
-  });
-}
+abstract class Tribe{}
