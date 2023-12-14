b5786c3ceeda7b8e5687f00bcaeaa6f3bbda3c1d
‘dh980502@gmail.com’ <dh980502@gmail.com>
2023-12-15 02:37:41 +0900

    연습문제2

--- lib/23_12_14/quest_2.dart
+++ lib/23_12_14/quest_2.dart
@@ -5,12 +5,13 @@ import 'package:http/http.dart' as http;
 
 void main()async{
   Todo todo = await getTodo(1);
+  print(todo.toJson());
 
 
 }
 Future<Todo> getTodo(int id)async{
   final response = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/\$%7BmovieId%7D?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=$id'));
-  print(response.body);
+  //print(response.body);
   return Todo.fromJson(jsonDecode(response.body));
   
 }
